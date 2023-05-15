3cd1bd979aa551bbd5dc10778393707c5b4112ae
wael-morjen <waelmorjen@gmail.com>
2023-05-15 13:35:15 +0100

    Init

---
+++ Client/medical-chat-app/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelContainer, ChannelListContainer } from './components';
+
+
+const apiKey = 'rrv37jzveb58';
+
+const chatClient = StreamChat.getInstance(apiKey);
+
+const App = () => {
+  return (
+    <div className="app__wrapper">
+      <Chat client={chatClient} theme="team light">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
+    </div>
+  );
+}
+
+export default App
