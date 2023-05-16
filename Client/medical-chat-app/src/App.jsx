48f77bb4484bcd9389347868c76bfd104d9c787d
wael-morjen <waelmorjen@gmail.com>
2023-05-16 10:43:19 +0100

    Auth

--- Client/medical-chat-app/src/App.jsx
+++ Client/medical-chat-app/src/App.jsx
@@ -3,7 +3,7 @@ import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
-import { ChannelContainer, ChannelListContainer } from './components';
+import { ChannelContainer, ChannelListContainer, Auth } from './components';
 
 import './App.css'; 
 
@@ -11,7 +11,13 @@ const apiKey = 'rrv37jzveb58';
 
 const chatClient = StreamChat.getInstance(apiKey);
 
+const authToken = false;
+
+
 const App = () => {
+
+  if (!authToken) return <Auth />;
+  
   return (
     <div className="app__wrapper">
       <Chat client={chatClient} theme="team light">
--- Client/medical-chat-app/src/components/index.js
+++ Client/medical-chat-app/src/components/index.js
@@ -3,4 +3,5 @@ export { default as ChannelListContainer } from './ChannelListContainer';
 export { default as ChannelSearch } from './ChannelSearch';
 export { default as TeamChannelList } from './TeamChannelList';
 export { default as TeamChannelPreview } from './TeamChannelPreview';
+export { default as Auth } from './Auth';
 
