39f322d8efe406485e3210326d99308f8b88b61f
wael-morjen <waelmorjen@gmail.com>
2023-05-18 19:20:01 +0100

    fixed cookies + logout

--- Client/medical-chat-app/src/components/ChannelListContainer.jsx
+++ Client/medical-chat-app/src/components/ChannelListContainer.jsx
@@ -7,8 +7,10 @@ import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
 import HospitalIcon from '../assets/hospital.png';
 import LogoutIcon from '../assets/logout.png';
 
+const cookies = new Cookies();
 
-const SideBar = () => (
+
+const SideBar = ({ logout }) => (
     <div className="channel-list__sidebar">
         <div className="channel-list__sidebar__icon1">
             <div className="icon1__inner">
@@ -16,7 +18,7 @@ const SideBar = () => (
             </div>
         </div>
         <div className="channel-list__sidebar__icon2">
-            <div className="icon2__inner">
+            <div className="icon1__inner" onClick={logout}>
                 <img src={LogoutIcon} alt="Logout" width="30" />
             </div>
         </div>
@@ -33,9 +35,22 @@ const CompanyHeader = () => {
 
 
 const ChannelListContainer = () => {
+
+    const logout = () => {
+        cookies.remove('token');
+        cookies.remove('userId');
+        cookies.remove('username');
+        cookies.remove('fullName');
+        cookies.remove('phoneNumber');
+        cookies.remove('avatarURL');
+        cookies.remove('hashedPassword');
+
+        window.location.reload();
+    }
+
   return (
     <>
-        <SideBar />
+        <SideBar logout={logout}/>
         <div className="channel-list__list__wrapper">
             <CompanyHeader />
             <ChannelSearch />
