bd585bc618e38b432bb77d20ecd6f6e3717a6d59
wael-morjen <waelmorjen@gmail.com>
2023-05-18 16:01:57 +0100

    server-side auth

--- Client/medical-chat-app/src/components/TeamChannelList.jsx
+++ Client/medical-chat-app/src/components/TeamChannelList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { AddChannel } from '../assets';
 
-const TeamChannelList = ({children, error  = false, loading, type}) => {
+const TeamChannelList = ({children, error = false, loading, type}) => {
 
     if(error) {
         return type === 'team' ? (
